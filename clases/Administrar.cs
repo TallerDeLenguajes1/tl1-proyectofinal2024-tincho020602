766950c77fdb393ee518c722eb92e96162441f1e
tincho020602 <ezequiel.ibarra020602@gmail.com>
2024-07-16 15:29:16 -0300

    cree un nuevo metodo llamado juego y MostrarMenu

---
+++ clases/Administrar.cs
@@ -0,0 +1,26 @@
+using EspacioPersonajes;
+using EventoAleatorio;
+
+namespace EspacioJuego
+{ 
+class Juego
+{
+    //Atributos
+    private List<Personaje> ListaDePersonajes {get;set;}
+    private Personaje Jugador1 {get;set;}
+
+    private Personaje Jugador2 {get;set;}
+
+    private int Turno {get;set;}
+    
+    public void MostrarMenu(string[] opciones)
+    {
+        foreach(var opcion in opciones)
+        {
+            Console.WriteLine(opcion);
+        }
+    }
+
+}
+    
+}
